be49d1eab1a359f793a027eb2c78fa760eccc15c
Andrés N. Robalino <andres@androbtech.com>
2020-11-09 00:44:01 -0500

    Driven port with in memory tax rate.

--- src/com/andrab/flares/cobolino/PriceMaker.java
+++ src/com/andrab/flares/cobolino/PriceMaker.java
@@ -2,11 +2,26 @@ package com.andrab.flares.cobolino;
 
 interface ForCalculatingPrices {
     public int echoValue(int value);
+    public double getTaxForState(String state);
+}
+
+interface ForGettingTaxRates {
+    public double taxForState(String state);
 }
 
 public class PriceMaker implements ForCalculatingPrices {
-    @Override
+
+    private ForGettingTaxRates taxStore;
+
+    public void setTaxStore(ForGettingTaxRates taxes) {
+        this.taxStore = taxes;
+    }
+
     public int echoValue(int value) {
         return value;
+    }
+
+    public double getTaxForState(String state) {
+        return taxStore.taxForState(state);
     }
 }
--- test/com/andrab/flares/cobolino/PriceMakerTests.java
+++ test/com/andrab/flares/cobolino/PriceMakerTests.java
@@ -6,10 +6,25 @@ import static org.hamcrest.CoreMatchers.equalTo;
 
 import org.junit.Test;
 
+class InMemoryTaxStore implements ForGettingTaxRates {
+    public double taxForState(String state) {
+        return 6.85;
+    }
+}
+
 public class PriceMakerTests {
     @Test
     public void ShouldEchoValue() {
         PriceMaker app = new PriceMaker();
         assertThat(app.echoValue(100), is(equalTo(100)));
+    }
+
+    @Test
+    public void ShouldGive6PercentForFlorida() {
+        PriceMaker app = new PriceMaker();
+        app.setTaxStore(new InMemoryTaxStore());
+
+        double tax = app.getTaxForState("FL");
+        assertThat(String.format("%.2f", tax), is(equalTo("6.85")));
     }
 }
